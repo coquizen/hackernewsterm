301c83d135f0fbc4c2b99a84d264187704446458
Ian G Canino <ian.canino@gmail.com>
2018-11-24 07:31:30 -0500

    implemented working but flawed comment tree population

--- gui.go
+++ gui.go
@@ -16,7 +16,7 @@ import (
 
 var wg = sync.WaitGroup{}
 var (
-	//cache   [50]*hnapi.Item
+	cache   []hnapi.Item
 	numCols int
 )
 
@@ -25,6 +25,7 @@ type GUI struct {
 	list     *tview.List
 	content  *tview.TextView
 	comments *tview.TreeView
+	console  *tview.TextView
 }
 
 // Create establishes the ui and widget parameters
@@ -32,6 +33,7 @@ func (gui *GUI) Create() {
 	gui.list = tview.NewList()
 	gui.list.ShowSecondaryText(true)
 	gui.list.SetBorder(true)
+	gui.list.SetChangedFunc(updateDisplay)
 
 	gui.content = tview.NewTextView()
 	gui.content.SetDynamicColors(true).
@@ -40,21 +42,32 @@ func (gui *GUI) Create() {
 	_, _, numCols, _ = gui.content.GetInnerRect()
 
 	gui.comments = tview.NewTreeView()
+	placeNode := tview.NewTreeNode(".")
+	gui.comments.SetRoot(placeNode)
+
+	gui.console = tview.NewTextView()
+	gui.console.SetDynamicColors(true)
+
 
 	var defaultRequest = &hnapi.Request{
 		PostType: "top",
 		NumPosts: 50,
 	}
 
-	go gui.getPosts(defaultRequest)
+	go func(req *hnapi.Request) {
+		gui.getPosts(defaultRequest)
+	}(defaultRequest)
 
 	gui.layout = tview.NewFlex()
-	gui.layout.SetDirection(tview.FlexColumn)
-	gui.layout.AddItem(gui.list, 0, 1, true)
+	gui.layout.SetDirection(tview.FlexRow)
 	gui.layout.AddItem(tview.NewFlex().
-		SetDirection(tview.FlexRow).AddItem(gui.content, 0, 1, false).
-		AddItem(gui.comments, 0, 1, false), 0, 1, false)
-
+		SetDirection(tview.FlexColumn).
+		AddItem(gui.list, 0, 1, true).
+		AddItem(tview.NewFlex().
+			SetDirection(tview.FlexRow).
+			AddItem(gui.content, 0, 1, false).
+			AddItem(gui.comments, 0, 1, false), 0, 1, false), 0, 1, true)
+	gui.layout.AddItem(gui.console, 1, 1, false)
 }
 
 func (gui *GUI) KeyHandler(key *tcell.EventKey) *tcell.EventKey {
@@ -67,30 +80,32 @@ func (gui *GUI) KeyHandler(key *tcell.EventKey) *tcell.EventKey {
 
 func (gui *GUI) getPosts(request *hnapi.Request) {
 
-func (gui *GUI) getPosts(request *hnapi.Request) {
-
 	gui.list.SetTitle(request.PostType + " stories")
 
 	idx := 0
 
 	stream := app.api.GetPosts(request)
-	cache := make([]*hnapi.Item, request.NumPosts)
-
+	cache = make([]hnapi.Item, request.NumPosts)
 	for item := range stream {
-		cache[idx] = item
-		gui.render(*cache[idx], idx)
-		if idx == 0 {
-			gui.parseHTML(*cache[idx])
-			gui.comments = gui.germinate(cache[idx])
-		}
+		cache[idx] = *item
+		gui.renderListItem(cache[idx], idx)
 		idx++
 	}
 }
 
-func (gui *GUI) render(item hnapi.Item, idx int) {
-	mainString := []string{"[[yellow:b]", strconv.Itoa(idx), "[-:-:-]] ", formatMainText(&item)}
+func updateDisplay(index int, _ string, _ string, _ rune) {
+	go func() {
+		parseHTML(cache[index])
+		germinate(cache[index])
+	}()
+	app.main.Draw()
+}
+
+func (gui *GUI) renderListItem(item hnapi.Item, idx int) {
+	mainString := []string{"[yellow:-]", strconv.Itoa(idx), "[-:-:-] ", formatMainText(&item)}
 	mainText := strings.Join(mainString, "")
 	secondaryText := formatSubText(&item)
+	_, _ = gui.console.Write([]byte(mainText))
 	gui.list.AddItem(mainText, secondaryText, rune(idx+1), nil)
 }
 
@@ -121,80 +136,56 @@ func formatSubText(item *hnapi.Item) string {
 	return strings.Join(str, "")
 }
 
-func (gui *GUI) parseHTML(item hnapi.Item) {
-
-	gui.content.SetTitle(item.URL)
-
+func parseHTML(item hnapi.Item) {
+	app.gui.content.Clear()
+	app.gui.content.SetTitle(item.URL)
 	webCMD := exec.Command("w3m", "-dump", "-graph", "-X", "-cols", string(numCols), item.URL)
 
 	app.main.QueueUpdateDraw(func() {
 		stdOutput, _ := webCMD.CombinedOutput()
-		_, err := gui.content.Write(stdOutput)
+		_, err := app.gui.content.Write(stdOutput)
 		if err != nil {
 			log.Print(err)
 		}
 	})
 	app.main.Draw()
 }
+
 // Adapted from github.com/johnshiver/plankton/terminal/treeview.go
-func createAllChildNodes(nodeID int) *tview.TreeNode {
-	var addNode func(id int) *tview.TreeNode
-	addNode = func(nodeID int) *tview.TreeNode {
-		comment, _ := app.api.GetItem(nodeID)
-		commentText := fmt.Sprintf("[::b]%s[::d] (%d) [-:-:-]--[-:-:-]%s", comment.By, comment.Time, comment.Text)
-		newNode := *tview.NewTreeNode(commentText)
-		for _, childID := range comment.Kids {
-			childComment, _ := app.api.GetItem(childID)
-			cNode := addNode(childComment.ID)
-			newNode.AddChild(cNode)
+func createAllChildNodes(parentItem *hnapi.Item) *tview.TreeNode {
+	var addNode func(item *hnapi.Item) *tview.TreeNode
+	addNode = func(item *hnapi.Item) *tview.TreeNode {
+		commentText := fmt.Sprintf("[::b]%s[::d] (%d) [-:-:-]-- %s", item.By, item.Time, item.Text)
+		commentNode := *tview.NewTreeNode(commentText).SetReference(item.ID)
+		for _, childID := range item.Kids {
+			childItem, _ := app.api.GetItem(childID)
+			childNode := addNode(childItem)
+			commentNode.AddChild(childNode)
 		}
-		return &newNode
+		return &commentNode
 	}
-	return addNode(nodeID)
+	return addNode(parentItem)
 }
 
-func (gui *GUI) germinate(item *hnapi.Item) (tree *tview.TreeView) {
-	for id := range item.Kids {
-		rootComment, _ := app.api.GetItem(id)
-		tree := tview.NewTreeView()
-		rootNode := createAllChildNodes(id)
+func germinate(item hnapi.Item) {
+	app.gui.layout.SetTitle("Comments")
+	var topLevelNode *tview.TreeNode
+	topLevelNode = tview.NewTreeNode("Comments")
+
+	for childID := range item.Kids {
+		rootComment, _ := app.api.GetItem(childID)
+		rootNode := createAllChildNodes(rootComment)
 		rootNode.SetExpanded(false)
-
-		tree.SetBorder(true).
-			SetTitle(rootComment.By)
-		tree.SetAlign(false).
-			SetTopLevel(0).
-			SetGraphics(true).
-			SetPrefixes(nil)
-
-		var add func(targetNode *tview.TreeNode) *tview.TreeNode
-		add = func(targetNode *tview.TreeNode) *tview.TreeNode {
-			commentText := fmt.Sprintf("[::b]%s[::d] (%d) [-:-:-]--[-:-:-]%s", rootComment.By, rootComment.Time, rootComment.Text)
-			commentNode := tview.NewTreeNode(commentText).
-				SetSelectable(true).
-				SetExpanded(targetNode == rootNode).
-				SetReference(targetNode)
-			if targetNode.IsExpanded() {
-				targetNode.SetColor(tcell.ColorGreen)
-			}
-
-			for _, childNode := range rootNode.GetChildren() {
-				commentNode.AddChild(add(childNode))
-			}
-			return commentNode
-		}
-
-		root := add(rootNode)
-		tree.SetRoot(root).
-			SetCurrentNode(root).
-			SetSelectedFunc(func(n *tview.TreeNode) {
-				original := n.GetReference().(*tview.TreeNode)
-				if original.IsExpanded() {
-					n.SetExpanded(!n.IsExpanded())
-				}
-			})
-		tree.GetRoot().ExpandAll()
-
+		rootNode.SetReference(childID)
+		commentText := fmt.Sprintf("[blue::b]%s[-::d] -- %s", rootComment.By, rootComment.Text)
+		rootNode.SetText(commentText)
+		topLevelNode.AddChild(rootNode)
 	}
-	return tree
+	app.gui.comments.SetRoot(topLevelNode)
+	// 	SetSelectedFunc(func(n *tview.TreeNode) {
+	// 		original := n.GetReference().(*tview.TreeNode)
+	// 		if original.IsExpanded() {
+	// 			n.SetExpanded(!n.IsExpanded())
+	// 		}
+	app.main.Draw()
 }
